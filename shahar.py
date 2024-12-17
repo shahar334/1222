6c21d38319da1a58b568f139fbfc97f947b5c9de
shahar334 <shahar.hanuona2112@gmail.com>
2024-12-17 19:54:46 +0200

    childhood story

--- shahar.py
+++ shahar.py
@@ -1,3 +1,7 @@
 def print_funny_story():
     print("nfaltey al hrizpa")
 print_funny_story()
+
+def print_childhood_story():
+    print("when i was child u was eaten a ants ")
+print_childhood_story()
